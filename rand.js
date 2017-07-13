481e40027f0622def8658b7cdd527e1c22befe0b
4446o89j <andreas.ramdas2000@gmail.com>
2017-07-13 06:19:35 -0700

    created a random number generator

---
+++ rand.js
@@ -0,0 +1,7 @@
+function rand(number){
+    var randomDecimalNumber = Math.random()*number;
+    console.log(randomDecimalNumber);
+    var wholeIntegerNumber = Math.round(randomDecimalNumber);
+    return wholeIntegerNumber;
+}
+console.log(rand(6));
